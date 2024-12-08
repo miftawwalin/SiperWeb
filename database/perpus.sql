d116375d8f8ee23fa6a2dbb2ee0029a700833997
Mifta Awwaliin <miftahalawwal@gmail.com>
2024-12-08 09:30:01 +0700

    merubah perpus.sql

--- database/perpus.sql
+++ database/perpus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2024 at 06:09 PM
+-- Generation Time: Dec 08, 2024 at 03:25 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,6 +41,49 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id_user`, `nama_user`, `username`, `password`) VALUES
 ('01', 'Miftahul AI', 'mifta41', 'admin1'),
 ('o2', 'Miftahul AI', 'mifta41@gmail.com', 'mifta41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usersignup`
+--
+
+CREATE TABLE `usersignup` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usersignup`
+--
+
+INSERT INTO `usersignup` (`id`, `username`, `email`, `password`, `created_at`) VALUES
+(1, 'Miftahul AI', 'mifta@gmail.com', '$2y$10$ndTVEiSDejJNHZ438mLhKeHl8cI1STWp8GIIHbbQ9PtrqVRNucD5a', '2024-12-02 17:50:09'),
+(2, 'Reka Permana', 'rekka21@gmail.com', '$2y$10$VGuv/P0xpgkdMb3j1PHRi.wujjIASX9pPrHjYOhCT/xOEGE7r1.o2', '2024-12-05 18:06:26');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usersignup`
+--
+ALTER TABLE `usersignup`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usersignup`
+--
+ALTER TABLE `usersignup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
